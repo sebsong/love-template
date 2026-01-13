77ac67948bdaf9b25ae2e01089d73f1c57dbacc2
Sebastian Song <seby.song@gmail.com>
2026-01-12 16:47:38 -0800

    clear out readme and license; restructure folders; add some sample starter code

--- game/main.lua
+++ game/main.lua
@@ -1,9 +1,17 @@
+if arg[2] == "debug" then
+    require("lldebugger").start()
+end
+
 function love.load()
 end
 
 function love.update()
-	print("Hello!")
+	if love.keyboard.isDown("escape") then
+		love.event.quit()
+	end
 end
 
 function love.draw()
+	local width, height = love.graphics.getDimensions()
+	love.graphics.print("slurp's up", width / 2, height / 2)
 end
